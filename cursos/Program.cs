092bc55e4309fc6f85f44e149ed82fc6d1ccfcdb
arthu <arthu@ARTHUR>
2024-07-24 19:58:29 -0300

    Adicionar arquivos de projeto.

---
+++ cursos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main()
+    {
+        
+        Console.WriteLine("Quanto é 1 + 1?");
+        int resposta1 = int.Parse(Console.ReadLine());
+
+        if (resposta1 == 2)
+        {
+            Console.WriteLine("Você acertou!");
+        }
+        else
+        {
+            Console.WriteLine("Você errou!");
+        }
+
+        
+        Console.WriteLine("Quanto é 2 + 2?");
+        int resposta2 = int.Parse(Console.ReadLine());
+
+        if (resposta2 == 4)
+        {
+            Console.WriteLine("Acertou em CHEIO!");
+        }
+        else
+        {
+            Console.WriteLine("Infelizmente errou.");
+        }
+
+        
+        Console.WriteLine("Quanto é 2 + 3?");
+        int resposta3 = int.Parse(Console.ReadLine());
+
+        if (resposta3 == 5)
+        {
+            Console.WriteLine("MUITO BEM!");
+        }
+        else
+        {
+            Console.WriteLine("Errou :(");
+        }
+    }
+}
